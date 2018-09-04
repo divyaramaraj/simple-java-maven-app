43459edd5eb8b9d41d22e77485678a79f9632bf7
divyaramaraj <42750592+divyaramaraj@users.noreply.github.com>
2018-09-04 09:47:13 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World - build 2 test";
+    private final String message = "Hello World build 2 test";
 
     public App() {}
 
